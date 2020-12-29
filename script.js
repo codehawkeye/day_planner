99d16643460d3ad35af5af6b6c99694b9dad7371
Mike Carter <elite.mikecarter@gmail.com>
2020-12-28 19:16:44 -0500

    fixed local storage and console issue

--- script.js
+++ script.js
@@ -1,65 +1,93 @@
-// load up the HTML first!!!!!
-$(document).ready(function () {
-    // Get the date with moment
-    console.log(moment().format("dddd, MMMM Do"))
-    // Put date on page 
-    $("#currentDay").text(moment().format("dddd, MMMM Do"))
-
-    // get the current hour
-    var currentHour = moment().hours();
+$(document).ready(function() {
+    var events = [];
     
-    // make a forEach loop that goes over the hour blocks
-    $(".time-block").each(function () {
-
-        // get the value from the HTML for the current hour block    
-        var blockHour = parseInt($(this).attr("id").split("-")[1]);
-
-        //    check to see if currentHour is greater > or < blockHour
-        if (blockHour < currentHour) {
-            // add CSS classes according to time
-            $(this).addClass("past");
-            
-        }
-        else if (blockHour === currentHour) {
-            // add CSS classes according to time
-            $(this).removeClass("past");
-            $(this).addClass("present");
-        }
-        else {
-            // add CSS classes according to time
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
-        }
+    // listen for save button clicks
+    $(".saveBtn").on("click", function() {
+      // get nearby values
+      var value = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+      var dateAdded = moment().format("dddd, MMMM Do");
+  
+      events.push({description: value, time: time, date: dateAdded});
+  
+      // save the value in localStorage as time
+      localStorage.setItem("events", JSON.stringify(events));
+      
     });
-
-}
-    
-    hourUpdater();
-
-    // set up interval to check if current time needs to be updated
-var interval = setInterval(hourUpdater, 15000);
-
-// load and saved data from localStorage
-$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-$("#hour-17 .description").val(localStorage.getItem("hour-17"));
-
-   
-
-    
-   
+  
+    function hourUpdater() {
+      // get current number of hours
+      var currentHour = moment().hours();
+  
+      // loop over time blocks
+      $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+  
+        // check if we've moved past this time
+        // if the current hour is greater than the block hour
+        // then add class "past"
+        if(currentHour > blockHour) {
+          $(this).addClass("past");
+        }
+        // if they are equal
+        // then remove class "past" and add class "present"
+        else if(currentHour === blockHour) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+        }
+        // else
+        // remove class "past", remove class "present", add class "future"
+        else {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        }
         
+      });
+    }
+  
+    hourUpdater();
+  
+    // set up interval to check if current time needs to be updated
+    // which means execute hourUpdater function every 15 seconds
+    var secondsLeft = 15;
+    function setTime() {
+      setInterval(function() {
+        secondsLeft--;
     
-
+        if(secondsLeft === 0) {
+          hourUpdater();
+          secondsLeft = 15;
+        }
     
-    
-    
-
-})
+      }, 1000);
+    }
+    setTime();
+  
+    // reset on new day
+    var currentDay = moment().format("dddd, MMMM Do");
+    for(var i = 0; i < events.length; i++) {
+      if(currentDay.isAfter(events[i].date)) {
+        events[i].description = "";
+        events[i].time = "";
+        events[i].date = "";
+        events.length = 0;
+      }
+    }
+  
+    // load any saved data from localStorage
+    var storedEvents = JSON.parse(localStorage.getItem("events"));
+  
+    if (storedEvents !== null) {
+      events = storedEvents;
+    }
+  
+    for(var i = 0; i < events.length; i++) {
+      var userDescription = events[i].description;
+      $("#" + events[i].time).children(".description").text(userDescription);
+    }
+  
+  
+    // display current day on page
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+  });
